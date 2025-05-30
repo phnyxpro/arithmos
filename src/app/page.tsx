39ee89c91b17edeeff82d9a108dcffb5cc3ffb52
Stefan Rampersad <stefan@phnyx.pro>
2025-05-30 03:27:38 +0000

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,55 +81,69 @@ import {
 } from "@/app/landing-page-data";
 import { faqData } from "@/constants/faqData";
 
-// Define the mapping of calculator component names to their import functions
-const calculatorImportMap = {
-  BasicTimeCalculator: () => import("@/components/calculators/BasicTimeCalculator"),
-  SimplifiedPayrollCalculator: () => import("@/components/calculators/SimplifiedPayrollCalculator"),
-  SimplifiedLevyCalculator: () => import("@/components/calculators/SimplifiedLevyCalculator"),
-  SimpleVatCalculator: () => import("@/components/calculators/SimpleVatCalculator"),
-  ExciseDutyCalculator: () => import("@/components/calculators/ExciseDutyCalculator"),
-  GrossToNetSalaryCalculator: () => import("@/components/calculators/GrossToNetSalaryCalculator"),
-  OvertimePayCalculator: () => import("@/components/calculators/OvertimePayCalculator"),
-  BonusCommissionCalculator: () => import("@/components/calculators/BonusCommissionCalculator"),
-  VacationPayCalculator: () => import("@/components/calculators/VacationPayCalculator"),
-  LoanAmortisationCalculator: () => import("@/components/calculators/LoanAmortisationCalculator"),
-  MortgageCalculator: () => import("@/components/calculators/MortgageCalculator"),
-  SavingsInvestmentCalculator: () => import("@/components/calculators/SavingsInvestmentCalculator"),
-  CurrencyExchangeCalculator: () => import("@/components/calculators/CurrencyExchangeCalculator"),
-  SimpleInterestCalculator: () => import("@/components/calculators/SimpleInterestCalculator"),
-  MarkupMarginCalculator: () => import("@/components/calculators/MarkupMarginCalculator"),
-  BreakEvenCalculator: () => import("@/components/calculators/BreakEvenCalculator"),
-  CashFlowProjectionCalculator: () => import("@/components/calculators/CashFlowProjectionCalculator"),
-  DepreciationCalculator: () => import("@/components/calculators/DepreciationCalculator"),
-  TariffCustomsDutyCalculator: () => import("@/components/calculators/TariffCustomsDutyCalculator"),
-  FreightShippingCalculator: () => import("@/components/calculators/FreightShippingCalculator"),
-  CIFCalculator: () => import("@/components/calculators/CIFCalculator"),
-  StampDutyCalculator: () => import("@/components/calculators/StampDutyCalculator"),
-  PropertyTaxDialogCalculator: () => import("@/components/calculators/PropertyTaxDialogCalculator"),
-  RentalYieldCalculator: () => import("@/components/calculators/RentalYieldCalculator"),
-  AMLRiskCalculator: () => import("@/components/calculators/AMLRiskCalculator"),
-  FATCACRSCalculator: () => import("@/components/calculators/FATCACRSCalculator"),
-};
-
-// Define the type for the keys of the import map
-type CalculatorComponentName = keyof typeof calculatorImportMap;
+// Define the type for the keys of the calculator components
+type CalculatorComponentName = 
+  | 'BasicTimeCalculator'
+  | 'SimplifiedPayrollCalculator'
+  | 'SimplifiedLevyCalculator'
+  | 'SimpleVatCalculator'
+  | 'ExciseDutyCalculator'
+  | 'GrossToNetSalaryCalculator'
+  | 'OvertimePayCalculator'
+  | 'BonusCommissionCalculator'
+  | 'VacationPayCalculator'
+  | 'LoanAmortisationCalculator'
+  | 'MortgageCalculator'
+  | 'SavingsInvestmentCalculator'
+  | 'CurrencyExchangeCalculator'
+  | 'SimpleInterestCalculator'
+  | 'MarkupMarginCalculator'
+  | 'BreakEvenCalculator'
+  | 'CashFlowProjectionCalculator'
+  | 'DepreciationCalculator'
+  | 'TariffCustomsDutyCalculator'
+  | 'FreightShippingCalculator'
+  | 'CIFCalculator'
+  | 'StampDutyCalculator'
+  | 'PropertyTaxDialogCalculator'
+  | 'RentalYieldCalculator'
+  | 'AMLRiskCalculator'
+  | 'FATCACRSCalculator';
 
 // Define the type for the LazyComponentMap
 type LazyComponentMapTypes = {
   [K in CalculatorComponentName]: LazyExoticComponent<React.ComponentType<any>>;
 };
 
-// Create the LazyComponentMap using React.lazy and the import map
-const LazyComponentMap: LazyComponentMapTypes = Object.keys(calculatorImportMap).reduce((acc, key) => {
-  const componentName = key as CalculatorComponentName;
-  // For named exports, you need to import and then return an object with a default property
-  acc[componentName] = React.lazy(() => 
-    calculatorImportMap[componentName]().then(module => ({
-      default: module[componentName as keyof typeof module] as React.ComponentType<any>
-    }))
-  );
-  return acc;
-}, {} as LazyComponentMapTypes);
+// Create the LazyComponentMap using React.lazy with explicit named imports
+const LazyComponentMap: LazyComponentMapTypes = {
+  BasicTimeCalculator: React.lazy(() => import("@/components/calculators/BasicTimeCalculator").then(module => ({ default: module.BasicTimeCalculator }))),
+  SimplifiedPayrollCalculator: React.lazy(() => import("@/components/calculators/SimplifiedPayrollCalculator").then(module => ({ default: module.SimplifiedPayrollCalculator }))),
+  SimplifiedLevyCalculator: React.lazy(() => import("@/components/calculators/SimplifiedLevyCalculator").then(module => ({ default: module.SimplifiedLevyCalculator }))),
+  SimpleVatCalculator: React.lazy(() => import("@/components/calculators/SimpleVatCalculator").then(module => ({ default: module.SimpleVatCalculator }))),
+  ExciseDutyCalculator: React.lazy(() => import("@/components/calculators/ExciseDutyCalculator").then(module => ({ default: module.ExciseDutyCalculator }))),
+  GrossToNetSalaryCalculator: React.lazy(() => import("@/components/calculators/GrossToNetSalaryCalculator").then(module => ({ default: module.GrossToNetSalaryCalculator }))),
+  OvertimePayCalculator: React.lazy(() => import("@/components/calculators/OvertimePayCalculator").then(module => ({ default: module.OvertimePayCalculator }))),
+  BonusCommissionCalculator: React.lazy(() => import("@/components/calculators/BonusCommissionCalculator").then(module => ({ default: module.BonusCommissionCalculator }))),
+  VacationPayCalculator: React.lazy(() => import("@/components/calculators/VacationPayCalculator").then(module => ({ default: module.VacationPayCalculator }))),
+  LoanAmortisationCalculator: React.lazy(() => import("@/components/calculators/LoanAmortisationCalculator").then(module => ({ default: module.LoanAmortisationCalculator }))),
+  MortgageCalculator: React.lazy(() => import("@/components/calculators/MortgageCalculator").then(module => ({ default: module.MortgageCalculator }))),
+  SavingsInvestmentCalculator: React.lazy(() => import("@/components/calculators/SavingsInvestmentCalculator").then(module => ({ default: module.SavingsInvestmentCalculator }))),
+  CurrencyExchangeCalculator: React.lazy(() => import("@/components/calculators/CurrencyExchangeCalculator").then(module => ({ default: module.CurrencyExchangeCalculator }))),
+  SimpleInterestCalculator: React.lazy(() => import("@/components/calculators/SimpleInterestCalculator").then(module => ({ default: module.SimpleInterestCalculator }))),
+  MarkupMarginCalculator: React.lazy(() => import("@/components/calculators/MarkupMarginCalculator").then(module => ({ default: module.MarkupMarginCalculator }))),
+  BreakEvenCalculator: React.lazy(() => import("@/components/calculators/BreakEvenCalculator").then(module => ({ default: module.BreakEvenCalculator }))),
+  CashFlowProjectionCalculator: React.lazy(() => import("@/components/calculators/CashFlowProjectionCalculator").then(module => ({ default: module.CashFlowProjectionCalculator }))),
+  DepreciationCalculator: React.lazy(() => import("@/components/calculators/DepreciationCalculator").then(module => ({ default: module.DepreciationCalculator }))),
+  TariffCustomsDutyCalculator: React.lazy(() => import("@/components/calculators/TariffCustomsDutyCalculator").then(module => ({ default: module.TariffCustomsDutyCalculator }))),
+  FreightShippingCalculator: React.lazy(() => import("@/components/calculators/FreightShippingCalculator").then(module => ({ default: module.FreightShippingCalculator }))),
+  CIFCalculator: React.lazy(() => import("@/components/calculators/CIFCalculator").then(module => ({ default: module.CIFCalculator }))),
+  StampDutyCalculator: React.lazy(() => import("@/components/calculators/StampDutyCalculator").then(module => ({ default: module.StampDutyCalculator }))),
+  PropertyTaxDialogCalculator: React.lazy(() => import("@/components/calculators/PropertyTaxDialogCalculator").then(module => ({ default: module.PropertyTaxDialogCalculator }))),
+  RentalYieldCalculator: React.lazy(() => import("@/components/calculators/RentalYieldCalculator").then(module => ({ default: module.RentalYieldCalculator }))),
+  AMLRiskCalculator: React.lazy(() => import("@/components/calculators/AMLRiskCalculator").then(module => ({ default: module.AMLRiskCalculator }))),
+  FATCACRSCalculator: React.lazy(() => import("@/components/calculators/FATCACRSCalculator").then(module => ({ default: module.FATCACRSCalculator }))),
+};
 
 interface ActiveCalculatorInfo {
   key: string;

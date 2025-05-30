48358483bfc6d731196b8631d1475c6f53c3c5ac
Stefan Rampersad <stefan@phnyx.pro>
2025-05-30 20:24:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1747695162263.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev/_next/static/chunks/node_modules_c2051bcb._.js
        - https://6000-firebase-studio-1747695162263.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Remove black background of image.

--- src/components/icons/app-logo.tsx
+++ src/components/icons/app-logo.tsx
@@ -1,8 +1,18 @@
-import type { SVGProps } from 'react';
-import Image from 'next/image';
+import type { ImgHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
 
-export function AppLogo(props: SVGProps<SVGSVGElement>) {
+export function AppLogo(props: ImgHTMLAttributes<HTMLImageElement>) {
+  const { className, ...rest } = props;
   return (
-    <img src="https://firebasestorage.googleapis.com/v0/b/taxtt-h5fyu.firebasestorage.app/o/file.svg?alt=media&token=970ecef0-bfd9-4df4-911b-87c0ad9a5a06" alt="Arithmos Logo" className="h-6 w-auto" />
+    <img
+      src="https://firebasestorage.googleapis.com/v0/b/taxtt-h5fyu.firebasestorage.app/o/file.svg?alt=media&token=970ecef0-bfd9-4df4-911b-87c0ad9a5a06"
+      alt="Arithmos Logo"
+      className={cn(
+        "h-6 w-auto", // Default size, can be overridden by passed className
+        "bg-gray-100 p-0.5 rounded-sm", // Added light background, padding, and rounding
+        className // Apply passed className, allowing overrides
+      )}
+      {...rest}
+    />
   );
 }

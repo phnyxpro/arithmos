554a5f1706e85d030ffc82b1d44dc3b14999926a
Stefan Rampersad <stefan@phnyx.pro>
2025-06-03 14:16:58 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/ai/genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
 });
+
+enableFirebaseTelemetry();
